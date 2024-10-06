c6fd32993f3f19b29f316bc52c917cde8e2fe7a4
hanseldelapaz <hanseelita@gmail.com>
2024-10-06 21:33:03 +0800

    ex6.php

--- ex5.php
+++ ex5.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Validation Example</title>
-    <link rel="stylesheet" href="../css/ex5.css">
+    <link rel="stylesheet" href="ex6.css">
     
     
 </head>
